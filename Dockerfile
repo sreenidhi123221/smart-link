80c25dc8908ba9e2b0129bebaedf7011ac65afa3
sreenidhi123221 <yasosreenidhiyadala@gmail.com>
2026-08-02 10:26:54 +0530

    Ready for Render deployment

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-EXPOSE 10000
+EXPOSE 5000
 
-CMD ["gunicorn", "--bind", "0.0.0.0:10000", "app:app"]
+CMD ["gunicorn", "--bind", "0.0.0.0:5000", "app:app"]
--- app.py
+++ app.py
@@ -2,27 +2,20 @@ from flask import Flask, render_template, request, redirect
 from database import get_db_connection
 import random
 import string
-
+import os
 
 app = Flask(__name__)
 
-
-# Generate random short code
 def generate_short_code(length=6):
     characters = string.ascii_letters + string.digits
     return ''.join(random.choice(characters) for _ in range(length))
 
-
-# Home Page
-@app.route('/')
+@app.route("/")
 def home():
     return render_template("index.html")
 
-
-# Create Short URL
-@app.route('/shorten', methods=['POST'])
+@app.route("/shorten", methods=["POST"])
 def shorten():
-
     long_url = request.form.get("long_url")
 
     short_code = generate_short_code()
@@ -36,55 +29,40 @@ def shorten():
     """
 
     cursor.execute(query, (long_url, short_code))
-
     connection.commit()
 
     cursor.close()
     connection.close()
 
-
     short_url = f"{request.host_url}{short_code}"
-
 
     return render_template(
         "index.html",
         short_url=short_url
     )
 
-
-# Redirect short URL
-@app.route('/<short_code>')
+@app.route("/<short_code>")
 def redirect_url(short_code):
 
     connection = get_db_connection()
     cursor = connection.cursor()
 
-
     query = """
-    SELECT original_url 
+    SELECT original_url
     FROM links
     WHERE short_code = %s
     """
 
-
     cursor.execute(query, (short_code,))
-
     result = cursor.fetchone()
-
 
     cursor.close()
     connection.close()
 
-
     if result:
         return redirect(result[0])
 
-
     return "Short URL not found"
-
-
-
-import os
 
 if __name__ == "__main__":
     app.run(
